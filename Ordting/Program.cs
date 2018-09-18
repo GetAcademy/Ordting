2e34c8c44c191b88cce052e4efc731d65b6efc89
Terje Kolderup <terje@kolderup.net>
2018-09-18 12:21:39 +0200

    Add project files.

---
+++ Ordting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
